a445b3f9c2e700bbe55d2afb3de451987de2bf70
Sanjeet Prajapati <prajapatisanjeet50@gmail.com>
2024-08-29 23:34:04 +0530

    update: deployed to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app/', // Must match your GitHub repository name
+  base: '/weather-app/',
+  build: {
+      outDir: "dist",
+  },
 });

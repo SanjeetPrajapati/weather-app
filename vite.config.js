8c8f1bd86d43acf7bb8efd451a316c1e65132010
Sanjeet Prajapati <prajapatisanjeet50@gmail.com>
2024-08-29 23:24:48 +0530

    Deploying site on master branch

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-app/', // This should match the repository name
+  base: '/weather-app/', // Must match your GitHub repository name
 });
